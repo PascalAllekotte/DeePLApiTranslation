e95bfedce077dcfed8240b5af05a7540bdd85075
pascalallekotte <pascalallekotte1@web.de>
2024-03-28 18:00:56 +0100

    DeepL Api Simple Translation / MVVM / Retrofit / MoshiFactory / ViewBinding

--- app/src/main/java/com/example/deeplapitranslation/Data/Api/ApiInterface.kt
+++ app/src/main/java/com/example/deeplapitranslation/Data/Api/ApiInterface.kt
@@ -1,7 +1,6 @@
 package de.syntax.androidabschluss.data.remote
 
 import com.example.deeplapitranslation.Data.Api.Response.DeeplResponse
-import com.example.deeplapitranslation.utils.DEEPL_AUTH_KEY
 import de.syntax.androidabschluss.adapter.Request.DeeplRequest
 import retrofit2.Call
 import retrofit2.http.Body
--- app/src/main/java/com/example/deeplapitranslation/utils/AppConstant.kt
+++ app/src/main/java/com/example/deeplapitranslation/utils/AppConstant.kt
@@ -4,4 +4,4 @@ package com.example.deeplapitranslation.utils
 
 
 const val BASE_URL = " https://api-free.deepl.com"
-const val DEEPL_AUTH_KEY = "48329d96-cd7d-4bc4-ac7f-f9d0e666f2f9:fx"
+const val DEEPL_AUTH_KEY = "DEEPL API KEY"
